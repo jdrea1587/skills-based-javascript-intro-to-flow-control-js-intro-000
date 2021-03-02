7e6ca15ad4ec3aa13b8f950b119a1ddc26e1576b
Joshua Rea <joshdanielrea@gmail.com>
2021-03-02 01:38:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,11 +16,11 @@ function ageChecker(age) {
     else {return "You are a grownup";}
 }
 
-/*
+
 function ternaryTeenager(age) {
-if (age >=13 && age <=19 ) ? "You are a teenager" : "You are not a teenager"
+if (age >=13 && age <=19 ) ? "You are a teenager" : "You are not a teenager";
 }
-*/
+
 function switchAge(age) {
   switch (age) {
     case 13:
